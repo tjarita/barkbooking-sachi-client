cbbe455c1ad87b2450413bbf9bf23fccb73047f7
Toshiki Arita <tarita141@gmail.com>
2018-06-08 22:33:58 -0700

    Add API for getting some dog breeds

---
+++ src/services/pets/dog.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default class DogApi {
+    constructor() {
+        this.api = axios.create({
+            baseURL: 'https://dog.ceo/api',
+            timeout: 1000,
+        });
+    }
+
+    getAllDogBreeds() {
+        this.api.get('breeds/list/all')
+            .then(function (response) {
+                console.log(response.data.message);
+            })
+            .catch(function (error) {
+                console.log(error);
+            };
+    }
+}
