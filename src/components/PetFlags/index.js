ec7780dd95132c0bd2de61d51bffe1b664570770
Toshiki Arita <tarita141@gmail.com>
2018-06-12 20:11:52 -0700

    Remove class since we're not using state

--- src/components/PetFlags/index.js
+++ src/components/PetFlags/index.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Popup, Container, List, Icon } from 'semantic-ui-react';
 import _ from 'lodash';
 
-export default class PetFlags extends Component {
-    render() {
-        const flags = this.props.flags;
+export default function PetFlags(props = { flags: null }) {
+    const flags = props.flags;
 
-        return (
-            <List divided relaxed selection>
-                {
-                    _.map(flags, ({ id, name, description, reason, icon }) => (
-                        <List.Item key={id}>
-                            <Popup
-                                trigger={<Icon bordered name={icon.name} color={icon.color ? icon.color : 'black'} size='large'></Icon>}
-                                content={<Container text>{description}</Container>}
-                            ></Popup>
-                            <List.Content>
-                                <List.Header>
-                                    {name}
-                                </List.Header>
-                                <List.Description>
-                                    {reason}
-                                </List.Description>
-                            </List.Content>
-                        </List.Item>
-                    ))
-                }
-            </List >
-        );
-    }
-}
-
-PetFlags.defaultProps = {
-    flags: null
+    return (
+        <List divided relaxed selection>
+            {
+                _.map(flags, ({ id, name, description, reason, icon }) => (
+                    <List.Item key={id}>
+                        <Popup
+                            trigger={<Icon bordered name={icon.name} color={icon.color || 'black'} size='large'></Icon>}
+                            content={<Container text>{description}</Container>}
+                        ></Popup>
+                        <List.Content>
+                            <List.Header>
+                                {name}
+                            </List.Header>
+                            <List.Description>
+                                {reason}
+                            </List.Description>
+                        </List.Content>
+                    </List.Item>
+                ))
+            }
+        </List >
+    );
 }
