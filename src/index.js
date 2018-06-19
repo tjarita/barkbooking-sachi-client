d1fbd447ea48e23d57a8df1e2da29ba2d14812f4
Toshiki Arita <tarita141@gmail.com>
2018-06-18 21:29:13 -0700

    Add font awesome itself instead of using the semantic ui version

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import Home from './scenes/Home'
 import registerServiceWorker from './registerServiceWorker';
 
+import fontawesome from '@fortawesome/fontawesome';
+import fas from '@fortawesome/fontawesome-pro-solid';
+
+fontawesome.library.add(fas);
 ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
